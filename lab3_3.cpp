23f004cdd42155989aa2a2754b486250f3bd844b
lab314-18@labeng.local <lab314-18@labeng.local>
2019-12-27 16:47:58 +0700

    lab3_3 V.1

---
+++ lab3_3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+float a = 1;
+float b = 2;
+float c;
+float d =0;
+while(b<70){
+c = a/b;
+b = b+1;
+d = d+c;
+}
+cout<< d;
+    return 0;
+}
